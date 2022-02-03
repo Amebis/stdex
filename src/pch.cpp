364ed2cfd4a4b61a7ccfdd7b51f418fd5de89471
Simon Rozman <simon@rozman.si>
2022-02-03 13:17:59 +0100

    Declare as portable
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- src/pch.cpp
+++
@@ -1,6 +0,0 @@
-﻿/*
-    SPDX-License-Identifier: GPL-3.0-or-later
-    Copyright © 2016-2022 Amebis
-*/
-
-#include "pch.h"
--- src/pch.h
+++
@@ -1,8 +0,0 @@
-﻿/*
-    SPDX-License-Identifier: GPL-3.0-or-later
-    Copyright © 2016-2022 Amebis
-*/
-
-#pragma once
-
-#include "../include/stdex/idrec.h"
