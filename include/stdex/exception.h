be8cffc1094ab4203ad10cc00a6d7d411fa4d076
Simon Rozman <simon@rozman.si>
2023-03-06 16:07:31 +0100

    Add user_cancelled exception type
    Signed-off-by: Simon Rozman <simon@rozman.si>

---
+++ include/stdex/exception.h
@@ -0,0 +1,28 @@
+/*
+    SPDX-License-Identifier: MIT
+    Copyright © 2023 Amebis
+*/
+
+#pragma once
+
+#include "sal.h"
+#include <exception>
+
+namespace stdex
+{
+    ///
+    /// User cancelled exception
+    ///
+    class user_cancelled : public std::exception
+    {
+    public:
+        ///
+        /// Constructs an exception
+        ///
+        /// \param[in] msg  Error message
+        ///
+        user_cancelled(_In_opt_z_ const char *msg = nullptr) : exception(msg)
+        {
+        }
+    };
+}
