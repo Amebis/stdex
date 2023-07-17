9fb22c51db4e3e3d72bf3dde84ea49eb9e52ad1d
rozmansi <rozmansi@users.noreply.github.com>
2023-07-17 08:40:12 +0000

    deploy: cd61f8afe0f1109db5730c48481fec3b8d081ecd

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['parser_5fcollection_0',['parser_collection',['../classstdex_1_1parser_1_1parser__collection.html',1,'stdex::parser']]],
-  ['progress_1',['progress',['../classstdex_1_1progress.html',1,'stdex']]]
+  ['progress_1',['progress',['../classstdex_1_1progress.html',1,'stdex']]],
+  ['progress_5fswitcher_2',['progress_switcher',['../classstdex_1_1progress__switcher.html',1,'stdex']]]
 ];
