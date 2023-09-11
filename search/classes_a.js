5b528c2e3d4a4226f398ac87c31979d3e460a1c3
rozmansi <rozmansi@users.noreply.github.com>
2023-09-11 15:42:35 +0000

    deploy: 1e993c8c65d19d1fddd546931e793452d47ff7f8

--- search/classes_a.js
+++ search/classes_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['mapping_0',['mapping',['../structstdex_1_1mapping.html',1,'stdex']]],
-  ['memory_5ffile_1',['memory_file',['../classstdex_1_1stream_1_1memory__file.html',1,'stdex::stream']]]
+  ['math_1',['math',['../class_unit_tests_1_1math.html',1,'UnitTests']]],
+  ['memory_5ffile_2',['memory_file',['../classstdex_1_1stream_1_1memory__file.html',1,'stdex::stream']]]
 ];
