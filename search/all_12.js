4350b27a487d6b41b51eb09a88e885644aafb840
rozmansi <rozmansi@users.noreply.github.com>
2023-09-13 11:22:00 +0000

    deploy: 180f76318487d118d1ad42d94584e5876c1193dd

--- search/all_12.js
+++ search/all_12.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['unlock_0',['unlock',['../classstdex_1_1stream_1_1basic__file.html#adf1a39e7aec3f14fedf6ce5c0984a791',1,'stdex::stream::basic_file::unlock()'],['../classstdex_1_1stream_1_1file__window.html#ac3d9a3cf3636010b43612d2c298e7c3d',1,'stdex::stream::file_window::unlock()'],['../classstdex_1_1stream_1_1cache.html#a7a15ab17abc847b380b13b43b4f1d12e',1,'stdex::stream::cache::unlock()'],['../classstdex_1_1stream_1_1file.html#a28e6d46e4183e094ef9fdcf591ebec46',1,'stdex::stream::file::unlock()'],['../classstdex_1_1stream_1_1diag__file.html#a9f0c158f4875c862f981282b818e7296',1,'stdex::stream::diag_file::unlock()']]],
-  ['user_5fcancelled_1',['user_cancelled',['../classstdex_1_1user__cancelled.html',1,'stdex::user_cancelled'],['../classstdex_1_1user__cancelled.html#a69f77539ead37dbd249143ad4b4048bd',1,'stdex::user_cancelled::user_cancelled()']]]
+  ['user_5fcancelled_1',['user_cancelled',['../classstdex_1_1user__cancelled.html',1,'stdex::user_cancelled'],['../classstdex_1_1user__cancelled.html#af2e1ebab1038ba4356e19c04f167d23d',1,'stdex::user_cancelled::user_cancelled()']]]
 ];
