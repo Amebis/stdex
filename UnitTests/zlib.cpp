c6f5a72e70f4e5445de39621a342a94b9fc19bbd
Simon Rozman <simon@rozman.si>
2024-02-21 15:51:21 +0100

    zlib: fix length comparison in unit test
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- UnitTests/zlib.cpp
+++ UnitTests/zlib.cpp
@@ -21,7 +21,7 @@ namespace UnitTests
 			zlib.write(inflated, sizeof(inflated) - sizeof(*inflated));
 		}
 		static const uint8_t deflated[] = { 0x78, 0xda, 0x0b, 0xc9, 0xc8, 0x2c, 0x56, 0x00, 0xa2, 0x44, 0x85, 0x92, 0xd4, 0xe2, 0x12, 0x3d, 0x00, 0x29, 0x97, 0x05, 0x24 };
-		Assert::AreEqual<size_t>(sizeof(deflated), dat_deflated.size());
+		Assert::AreEqual<stdex::stream::fsize_t>(sizeof(deflated), dat_deflated.size());
 		Assert::AreEqual(0, memcmp(deflated, dat_deflated.data(), sizeof(deflated)));
 
 		dat_deflated.seekbeg(0);
@@ -30,7 +30,7 @@ namespace UnitTests
 			stdex::zlib_reader zlib(dat_deflated, 3);
 			dat_inflated.write_stream(zlib);
 		}
-		Assert::AreEqual<size_t>(sizeof(inflated) - sizeof(*inflated), dat_inflated.size());
+		Assert::AreEqual<stdex::stream::fsize_t>(sizeof(inflated) - sizeof(*inflated), dat_inflated.size());
 		Assert::AreEqual(0, memcmp(inflated, dat_inflated.data(), sizeof(inflated) - sizeof(*inflated)));
 	}
 }
