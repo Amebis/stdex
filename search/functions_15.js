9b42d67bc0d7d884b6a94136b87f36299d2acc8a
rozmansi <rozmansi@users.noreply.github.com>
2024-03-01 08:29:30 +0000

    deploy: 7e07d14de7a69000d58db5b4ede1b89869e7c38d

--- search/functions_15.js
+++ search/functions_15.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['_7ebenchmark_0',['~benchmark',['../classstdex_1_1benchmark.html#ad84dd386789e370b4a8113d7bc39f663',1,'stdex::benchmark']]],
-  ['_7evector_5fqueue_1',['~vector_queue',['../classstdex_1_1vector__queue.html#a61460b8386f0ebc0a9c76db9951d342d',1,'stdex::vector_queue']]],
-  ['_7ewatchdog_2',['~watchdog',['../classstdex_1_1watchdog.html#a8ecedd7cd51129b3dc6775b638c91b16',1,'stdex::watchdog']]]
+  ['_7escoped_5fexecutor_1',['~scoped_executor',['../classstdex_1_1scoped__executor.html#aad57059fdd2f14cc22160fd043aeec2f',1,'stdex::scoped_executor']]],
+  ['_7evector_5fqueue_2',['~vector_queue',['../classstdex_1_1vector__queue.html#a61460b8386f0ebc0a9c76db9951d342d',1,'stdex::vector_queue']]],
+  ['_7ewatchdog_3',['~watchdog',['../classstdex_1_1watchdog.html#a8ecedd7cd51129b3dc6775b638c91b16',1,'stdex::watchdog']]]
 ];
