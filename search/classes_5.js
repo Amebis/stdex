1c397b8573c84b20ad8bda5762bf22ea082077cc
rozmansi <rozmansi@users.noreply.github.com>
2024-08-17 19:42:46 +0000

    deploy: a95409c9edbbcbd2e7788210304582ed396da650

--- search/classes_5.js
+++ search/classes_5.js
@@ -4,5 +4,6 @@ var searchData=
   ['file_1',['file',['../classstdex_1_1stream_1_1file.html',1,'stdex::stream']]],
   ['file_5fwindow_2',['file_window',['../classstdex_1_1stream_1_1file__window.html',1,'stdex::stream']]],
   ['format_3',['format',['../structstdex_1_1wav_1_1format.html',1,'stdex::wav']]],
-  ['free_5flocale_5fdelete_4',['free_locale_delete',['../structstdex_1_1free__locale__delete.html',1,'stdex']]]
+  ['free_5flocale_5fdelete_4',['free_locale_delete',['../structstdex_1_1free__locale__delete.html',1,'stdex']]],
+  ['freeaddrinfo_5fdelete_5',['freeaddrinfo_delete',['../structstdex_1_1freeaddrinfo__delete.html',1,'stdex']]]
 ];
