74718b9ef8672050204142825d65592bf65daf28
Simon Rozman <simon@rozman.si>
2024-10-21 12:50:52 +0200

    uuid: provide uuid_str_max constant for client code
    Using cryptic constants like 39 makes code awkward to understand.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- include/stdex/uuid.hpp
+++ include/stdex/uuid.hpp
@@ -21,23 +21,29 @@
 namespace stdex
 {
 	///
+	/// Number of characters required to hold a registry string {xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx} GUID representation.
+	/// Including zero terminator.
+	///
+	constexpr size_t uuid_str_max = 39;
+
+	///
 	/// Formats GUID to a registry string {xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx}.
 	///
-	/// \param[out] str  String to write GUID. Must point to at least 39 code points to write complete GUID including zero terminator.
+	/// \param[out] str  String to write GUID. Must point to at least `uuid_str_max` code points to write complete GUID including zero terminator.
 	/// \param[in ] id   GUID to write.
 	///
-	inline void uuidtostr(_Out_writes_z_(39) char str[39], _In_ const uuid_t& id)
+	inline void uuidtostr(_Out_writes_z_(uuid_str_max) char str[uuid_str_max], _In_ const uuid_t& id)
 	{
 		stdex_assert(str);
 #ifdef _WIN32
-		_snprintf_s_l(str, 39, _TRUNCATE, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", NULL,
+		_snprintf_s_l(str, uuid_str_max, _TRUNCATE, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", NULL,
 			id.Data1,
 			static_cast<unsigned int>(id.Data2),
 			static_cast<unsigned int>(id.Data3),
 			static_cast<unsigned int>(id.Data4[0]), static_cast<unsigned int>(id.Data4[1]),
 			static_cast<unsigned int>(id.Data4[2]), static_cast<unsigned int>(id.Data4[3]), static_cast<unsigned int>(id.Data4[4]), static_cast<unsigned int>(id.Data4[5]), static_cast<unsigned int>(id.Data4[6]), static_cast<unsigned int>(id.Data4[7]));
 #else
-		snprintf(str, 39, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
+		snprintf(str, uuid_str_max, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
 			*reinterpret_cast<const uint32_t*>(&id[0]),
 			static_cast<unsigned int>(*reinterpret_cast<const uint16_t*>(&id[4])),
 			static_cast<unsigned int>(*reinterpret_cast<const uint16_t*>(&id[6])),
@@ -49,21 +55,21 @@ namespace stdex
 	///
 	/// Formats GUID to a registry string {xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx}.
 	///
-	/// \param[out] str  String to write GUID. Must point to at least 39 code points to write complete GUID including zero terminator.
+	/// \param[out] str  String to write GUID. Must point to at least `uuid_str_max` code points to write complete GUID including zero terminator.
 	/// \param[in ] id   GUID to write.
 	///
-	inline void uuidtostr(_Out_writes_z_(39) wchar_t str[39], _In_ const uuid_t& id)
+	inline void uuidtostr(_Out_writes_z_(uuid_str_max) wchar_t str[uuid_str_max], _In_ const uuid_t& id)
 	{
 		stdex_assert(str);
 #ifdef _WIN32
-		_snwprintf_s_l(str, 39, _TRUNCATE, L"{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", NULL,
+		_snwprintf_s_l(str, uuid_str_max, _TRUNCATE, L"{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", NULL,
 			id.Data1,
 			static_cast<unsigned int>(id.Data2),
 			static_cast<unsigned int>(id.Data3),
 			static_cast<unsigned int>(id.Data4[0]), static_cast<unsigned int>(id.Data4[1]),
 			static_cast<unsigned int>(id.Data4[2]), static_cast<unsigned int>(id.Data4[3]), static_cast<unsigned int>(id.Data4[4]), static_cast<unsigned int>(id.Data4[5]), static_cast<unsigned int>(id.Data4[6]), static_cast<unsigned int>(id.Data4[7]));
 #else
-		swprintf(str, 39, L"{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", NULL,
+		swprintf(str, uuid_str_max, L"{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", NULL,
 			*reinterpret_cast<const uint32_t*>(&id[0]),
 			static_cast<unsigned int>(*reinterpret_cast<const uint16_t*>(&id[4])),
 			static_cast<unsigned int>(*reinterpret_cast<const uint16_t*>(&id[6])),
